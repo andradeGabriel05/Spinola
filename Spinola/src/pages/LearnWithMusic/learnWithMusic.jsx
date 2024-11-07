f4536bb92d83609767764cf32b9fe364c3d72bdd
Deploy PC <g.andrade0222@gmail.com>
2024-11-06 23:25:48 -0300

    v0.0.7 stating lyrics training on '/music-learning'

--- Spinola/src/pages/LearnWithMusic/learnWithMusic.jsx
+++ Spinola/src/pages/LearnWithMusic/learnWithMusic.jsx
@@ -1,28 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./learnWithMusic.scss";
 import { test } from "./test";
 
 export default function LearnWithMusic(video) {
-  useEffect(() => {
-    test();
-  });
-  const [buttonState, setButtonState] = useState("pause");
-  const [paused, setPaused] = useState(false);
-
+  const [buttonState, setButtonState] = useState("playing");
+  const [passedOnce, setPassedOnce] = useState(false);
+  const videoRef = useRef(null);
   function handlePauseResume() {
-    if (buttonState == "pause") {
-      setButtonState("play");
-      setPaused(true);
+    if (videoRef.current.paused) {
+    console.log(test(buttonState, passedOnce));
+      videoRef.current.play();
+      setButtonState("playing");
+
+      var tempoPreciso = test(buttonState, passedOnce) 
+      console.log(tempoPreciso / 1000)
+      videoRef.current.currentTime = tempoPreciso / 1000;
     } else {
+      videoRef.current.pause();
       setButtonState("pause");
-      setPaused(false);
-      test(paused);
+      setPassedOnce(true);
+
     }
   }
+
+  // Atualize o estado do vídeo e passe para o test
+  useEffect(() => {
+    test(buttonState, passedOnce);
+    if (buttonState === "playing") {
+      // setPassedOnce(true);
+    }
+  }, [buttonState, passedOnce]);
+
   return (
     <div className="container_music_video">
       <div className="wrapper_video_choice">
-        <video controls autoPlay={true} >
+        <video ref={videoRef} autoPlay={true}>
           <source
             src="/Françoise Hardy’s Message Personnel (1973).mp4"
             type="video/mp4"
@@ -30,11 +42,13 @@ export default function LearnWithMusic(video) {
         </video>
 
         <div className="wrapper_btns_choice">
-          <button onClick={test}>Choice1</button>
-          <button>Choice2</button>
-          <button>Choice3</button>
-          <button>Choice4</button>
-          <button onClick={handlePauseResume}>{buttonState}</button>
+          <button
+            onClick={() => {
+              handlePauseResume();
+            }}
+          >
+            {buttonState}
+          </button>
         </div>
       </div>
       <div className="lyrics" id="lyricsPageId"></div>
@@ -45,3 +59,27 @@ export default function LearnWithMusic(video) {
 {
   /* <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4" /> */
 }
+
+// } else if (videoState == "" && passedOnce == true) {
+//   lyricsData.lyricsTimeVerse.forEach((lista) => {
+//     console.log(Object.keys(lista)[0])
+//     const intervalId = setTimeout(() => {
+//       console.log(timeoutIds)
+//       console.log(timeoutIds[intervalId])
+//       time = Object.keys(lista)[0]
+//       console.log(time)
+//       lyricsPageId.innerHTML = "";
+//       const words = lista[Object.keys(lista)[0]].split(" ");
+//       words.forEach((word) => {
+//         const span = document.createElement("span");
+//         span.textContent = word;
+//         lyricsPageId.appendChild(span);
+//         console.log(timeoutIds)
+//       });
+//     }, Object.keys(lista)[0]);
+//     timeoutIds.push(intervalId);
+//   });
+// } else {
+//   timeoutIds.forEach((id) => clearTimeout(id));
+//   return passedOnce = true
+// }
--- Spinola/src/pages/LearnWithMusic/test.js
+++ Spinola/src/pages/LearnWithMusic/test.js
@@ -1,77 +1,128 @@
-export function test(videoState) {
+const timeoutIds = [];
+var time = 0;
+export function test(videoState, passedOnce) {
   const lyricsData = {
     lyricsTimeVerse: [
-      { time: 4282, verse: "Et il y a les mots que je ne dirai pas!" },
+      { "4282": "Et il y a les mots que je ne dirai pas" },
+      { "7855": "Tous ces mots qui font peur quand ils ne font pas rire" },
       {
-        time: 7855,
-        verse: "Tous ces mots qui font peur quand ils ne font pas rire!",
+        "11434": "Qui sont dans trop de films, de chansons et de livres",
+      },
+      { "15034": "Je voudrais vous les dire" },
+      { "19034": "Et je voudrais les vivre" },
+      { "22567": "Je ne le ferai pas" },
+      { "26034": "Je veux, je ne peux pas" },
+      { "30034": "Je suis seule à crever et je sais où vous êtes" },
+      {
+        "34534": "J'arrive, attendez-moi, nous allons nous connaître",
       },
       {
-        time: 11434,
-        verse: "Qui sont dans trop de films, de chansons et de livres!",
+        "39034": "Préparez votre temps, pour vous j'ai tout le mien",
       },
-      { time: 15034, verse: "Je voudrais vous les dire!" },
-      { time: 19034, verse: "Et je voudrais les vivre!" },
-      { time: 22567, verse: "Je ne le ferai pas!" },
-      { time: 26034, verse: "Je veux, je ne peux pas!" },
-      { time: 30034, verse: "Je suis seule à crever et je sais où vous êtes!" },
+      { "45034": "Je voudrais arriver, je reste, je me déteste" },
+      { "52034": "Je n'arriverai pas" },
+      { "55034": "Je veux, je ne peux pas" },
+      { "61034": "Je devrais vous parler" },
+      { "64034": "Je devrais arriver" },
+      { "67034": "Ou je devrais dormir" },
+      { "76034": "J'ai peur que tu sois sourd" },
+      { "78034": "J'ai peur que tu sois lâche" },
+      { "80534": "J'ai peur d'être indiscrète" },
       {
-        time: 34534,
-        verse: "J'arrive, attendez-moi, nous allons nous connaître!",
+        "88034": "Je ne peux pas vous dire que je t'aime peut-être",
       },
-      {
-        time: 39034,
-        verse: "Préparez votre temps, pour vous j'ai tout le mien!",
-      },
-      { time: 45034, verse: "Je voudrais arriver, je reste, je me déteste!" },
-      { time: 52034, verse: "Je n'arriverai pas!" },
-      { time: 55034, verse: "Je veux, je ne peux pas!" },
-      { time: 61034, verse: "Je devrais vous parler!" },
-      { time: 64034, verse: "Je devrais arriver!" },
-      { time: 67034, verse: "Ou je devrais dormir!" },
-      { time: 76034, verse: "J'ai peur que tu sois sourd!" },
-      { time: 78034, verse: "J'ai peur que tu sois lâche!" },
-      { time: 80534, verse: "J'ai peur d'être indiscrète!" },
-      {
-        time: 88034,
-        verse: "Je ne peux pas vous dire que je t'aime peut-être!",
-      },
-      { time: 90034, verse: "Mais si tu crois un jour que tu m'aimes!" },
-      { time: 94034, verse: "Ne crois pas que tes souvenirs me gênent!" },
-      { time: 99034, verse: "Et cours, cours jusqu'à perdre haleine!" },
-      { time: 104034, verse: "Viens me retrouver!" },
-      { time: 109034, verse: "Si tu crois un jour que tu m'aimes!" },
-      { time: 113034, verse: "Et si ce jour-là tu as de la peine!" },
-      { time: 118034, verse: "À trouver où tous ces chemins te mènent!" },
-      { time: 123034, verse: "Viens me retrouver!" },
-      { time: 130034, verse: "Si le dégoût de la vie vient en toi!" },
-      { time: 138033, verse: "Si la paresse de la vie s'installe en toi!" },
-      { time: 145034, verse: "Pense à moi!" },
-      { time: 149034, verse: "Pense à moi!" },
-      { time: 155034, verse: "Mais si tu crois un jour que tu m'aimes!" },
-      { time: 158034, verse: "Ne le considère pas comme un problème!" },
-      { time: 163034, verse: "Et cours et cours jusqu'à perdre haleine!" },
-      { time: 168034, verse: "Viens me retrouver!" },
-      { time: 173034, verse: "Si tu crois un jour que tu m'aimes!" },
-      { time: 177034, verse: "N'attends pas un jour, pas une semaine!" },
-      { time: 182034, verse: "Car tu ne sais pas où la vie t'amène!" },
-      { time: 187034, verse: "Viens me retrouver!" },
-      { time: 194034, verse: "Si le dégoût de la vie vient en toi!" },
-      { time: 202034, verse: "Si la paresse de la vie s'installe en toi!" },
-      { time: 209034, verse: "Pense à moi!" },
-      { time: 213034, verse: "Pense à moi!" },
+      { "90034": "Mais si tu crois un jour que tu m'aimes" },
+      { "94034": "Ne crois pas que tes souvenirs me gênent" },
+      { "99034": "Et cours, cours jusqu'à perdre haleine" },
+      { "104034": "Viens me retrouver" },
+      { "109034": "Si tu crois un jour que tu m'aimes" },
+      { "113034": "Et si ce jour-là tu as de la peine" },
+      { "118034": "À trouver où tous ces chemins te mènent" },
+      { "123034": "Viens me retrouver" },
+      { "130034": "Si le dégoût de la vie vient en toi" },
+      { "138033": "Si la paresse de la vie s'installe en toi" },
+      { "145034": "Pense à moi" },
+      { "149034": "Pense à moi" },
+      { "155034": "Mais si tu crois un jour que tu m'aimes" },
+      { "158034": "Ne le considère pas comme un problème" },
+      { "163034": "Et cours et cours jusqu'à perdre haleine" },
+      { "168034": "Viens me retrouver" },
+      { "173034": "Si tu crois un jour que tu m'aimes" },
+      { "177034": "N'attends pas un jour, pas une semaine" },
+      { "182034": "Car tu ne sais pas où la vie t'amène" },
+      { "187034": "Viens me retrouver" },
+      { "194034": "Si le dégoût de la vie vient en toi" },
+      { "202034": "Si la paresse de la vie s'installe en toi" },
+      { "209034": "Pense à moi" },
+      { "213034": "Pense à moi" },
     ],
   };
 
-  lyricsData.lyricsTimeVerse.forEach((lista) => {
-    setTimeout(() => {
-      lyricsPageId.innerHTML = "";
-      const words = lista.verse.split(" ");
-      words.forEach((word) => {
-        const span = document.createElement("span");
-        span.textContent = word;
-        lyricsPageId.appendChild(span);
-      });
-    }, lista.time);
-  });
+  if (videoState === "pause") {
+    timeoutIds.forEach((id) => clearTimeout(id));
+    timeoutIds.length = 0; // Limpa o array para remover referências aos IDs
+    console.log("Pausou");
+    console.log(time);
+    return time;
+  }
+
+  console.log("teste1");
+  if (!passedOnce) {
+    console.log("teste2");
+    lyricsData.lyricsTimeVerse.forEach((lista) => {
+      const intervalId = setTimeout(() => {
+        time = Number(Object.keys(lista)[0]);
+        console.log(time);
+        lyricsPageId.innerHTML = "";
+        const words = lista[Object.keys(lista)[0]].split(" ");
+        words.forEach((word) => {
+          const span = document.createElement("span");
+          span.textContent = word;
+          lyricsPageId.appendChild(span);
+        });
+      }, Object.keys(lista)[0]);
+      timeoutIds.push(intervalId);
+    });
+  } else if (passedOnce) {
+    console.log("teste3");
+    lyricsData.lyricsTimeVerse.forEach((lista) => {
+      const tempo = Number(Object.keys(lista)[0]);
+
+      const intervalId = setTimeout(() => {
+        time = tempo; // atualizar tempo como tempo que pausou e tempo que começa. Depois faz a subtração
+
+        lyricsPageId.innerHTML = "";
+        const words = lista[tempo].split(" ");
+        words.forEach((word) => {
+          const span = document.createElement("span");
+          span.textContent = word;
+          lyricsPageId.appendChild(span);
+        });
+
+      }, tempo - time); // subtração. Tempo que passou - tempo que pausou
+
+      timeoutIds.push(intervalId);
+    });
+  }
+  // } else if (videoState != "pause" && passedOnce) {
+  //   console.log("teste3");
+  //   lyricsData.lyricsTimeVerse.forEach((lista) => {
+  //     console.log(Object.keys(lista)[0]);
+  //     const intervalId = setTimeout(() => {
+  //       console.log(timeoutIds);
+  //       console.log(timeoutIds[intervalId]);
+  //       time = Object.keys(lista)[0];
+  //       console.log(time);
+  //       lyricsPageId.innerHTML = "";
+  //       const words = lista[Object.keys(lista)[0]].split(" ");
+  //       words.forEach((word) => {
+  //         const span = document.createElement("span");
+  //         span.textContent = word;
+  //         lyricsPageId.appendChild(span);
+  //         console.log(timeoutIds);
+  //       });
+  //     }, Object.keys(lista)[0]);
+  //     timeoutIds.push(intervalId);
+  //   });
+  // }
 }
