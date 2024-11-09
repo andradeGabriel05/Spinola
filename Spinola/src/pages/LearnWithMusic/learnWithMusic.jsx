63d50fdedc9bf61ef272a36006b2d5d34e6b4b7f
andradeGabriel05 <gabriel.andrade@sptech.school>
2024-11-09 20:23:11 -0300

    v0.0.7.4 improving learning music code

--- Spinola/src/pages/LearnWithMusic/learnWithMusic.jsx
+++ Spinola/src/pages/LearnWithMusic/learnWithMusic.jsx
@@ -2,39 +2,49 @@ import { useEffect, useRef, useState } from "react";
 import "./learnWithMusic.scss";
 import { test } from "./test";
 
-export default function LearnWithMusic(video) {
+export default function LearnWithMusic() {
   const [buttonState, setButtonState] = useState("playing");
   const [passedOnce, setPassedOnce] = useState(false);
+  const [tempoPreciso, setTempoPreciso] = useState(0); // Estado para observar o valor de `time`
   const videoRef = useRef(null);
+
   function handlePauseResume() {
     if (videoRef.current.paused) {
-    console.log(test(buttonState, passedOnce));
+      const tempo = test(buttonState, passedOnce);
+      console.log(tempo);
       videoRef.current.play();
       setButtonState("playing");
 
-      var tempoPreciso = test(buttonState, passedOnce) 
-      console.log(tempoPreciso / 1000)
-      videoRef.current.currentTime = tempoPreciso / 1000;
+      setTempoPreciso(tempo); // Define o valor de tempo
+      videoRef.current.currentTime = tempo / 1000;
     } else {
       videoRef.current.pause();
       setButtonState("pause");
       setPassedOnce(true);
-
     }
   }
 
-  // Atualize o estado do vídeo e passe para o test
   useEffect(() => {
-    test(buttonState, passedOnce);
-    if (buttonState === "playing") {
-      // setPassedOnce(true);
+    // Chama `test` e define o valor retornado em `tempoPreciso`
+    const tempo = test(buttonState, passedOnce);
+    setTempoPreciso(tempo);
+
+    // Verifica se `tempoPreciso` é suficiente para pausar o vídeo
+    if (
+      videoRef.current &&
+      videoRef.current.currentTime > tempoPreciso / 1000
+    ) {
+      videoRef.current.pause();
+      setButtonState("pause");
+      setPassedOnce(true);
+      console.log("Vídeo pausado no tempo especificado.");
     }
-  }, [buttonState, passedOnce]);
+  }, [buttonState, passedOnce, tempoPreciso]);
 
   return (
     <div className="container_music_video">
       <div className="wrapper_video_choice">
-        <video ref={videoRef} autoPlay={true}>
+        <video id="videoMusic" ref={videoRef} autoPlay={true}>
           <source
             src="/Françoise Hardy’s Message Personnel (1973).mp4"
             type="video/mp4"
--- Spinola/src/pages/LearnWithMusic/test.js
+++ Spinola/src/pages/LearnWithMusic/test.js
@@ -1,141 +1,165 @@
 const timeoutIds = [];
 var time = 0;
+var currentTime = 0;
+const valoresAtuais = [];
+var currentVerseIndex = 0;
+
 export function test(videoState, passedOnce) {
   const lyricsData = {
     lyricsTimeVerse: [
-      { "4282": "Et il y a les mots que je ne dirai pas" },
-      { "7855": "Tous ces mots qui font peur quand ils ne font pas rire" },
+      // 4282
+      { 0: "Et il y a les mots que je ne dirai pas" },
+      { 7855: "Tous ces mots qui font peur quand ils ne font pas rire" },
       {
-        "11434": "Qui sont dans trop de films, de chansons et de livres",
+        11434: "Qui sont dans trop de films, de chansons et de livres",
       },
-      { "15034": "Je voudrais vous les dire" },
-      { "19034": "Et je voudrais les vivre" },
-      { "22567": "Je ne le ferai pas" },
-      { "26034": "Je veux, je ne peux pas" },
-      { "30034": "Je suis seule à crever et je sais où vous êtes" },
+      { 15034: "Je voudrais vous les dire" },
+      { 19034: "Et je voudrais les vivre" },
+      { 22567: "Je ne le ferai pas" },
+      { 26034: "Je veux, je ne peux pas" },
+      { 30034: "Je suis seule à crever et je sais où vous êtes" },
       {
-        "34534": "J'arrive, attendez-moi, nous allons nous connaître",
+        34534: "J'arrive, attendez-moi, nous allons nous connaître",
       },
       {
-        "39034": "Préparez votre temps, pour vous j'ai tout le mien",
+        39034: "Préparez votre temps, pour vous j'ai tout le mien",
       },
-      { "45034": "Je voudrais arriver, je reste, je me déteste" },
-      { "52034": "Je n'arriverai pas" },
-      { "55034": "Je veux, je ne peux pas" },
-      { "61034": "Je devrais vous parler" },
-      { "64034": "Je devrais arriver" },
-      { "67034": "Ou je devrais dormir" },
-      { "76034": "J'ai peur que tu sois sourd" },
-      { "78034": "J'ai peur que tu sois lâche" },
-      { "80534": "J'ai peur d'être indiscrète" },
+      { 45034: "Je voudrais arriver, je reste, je me déteste" },
+      { 52034: "Je n'arriverai pas" },
+      { 55034: "Je veux, je ne peux pas" },
+      { 61034: "Je devrais vous parler" },
+      { 64034: "Je devrais arriver" },
+      { 67034: "Ou je devrais dormir" },
+      { 76034: "J'ai peur que tu sois sourd" },
+      { 78034: "J'ai peur que tu sois lâche" },
+      { 80534: "J'ai peur d'être indiscrète" },
       {
-        "88034": "Je ne peux pas vous dire que je t'aime peut-être",
+        88034: "Je ne peux pas vous dire que je t'aime peut-être",
       },
-      { "90034": "Mais si tu crois un jour que tu m'aimes" },
-      { "94034": "Ne crois pas que tes souvenirs me gênent" },
-      { "99034": "Et cours, cours jusqu'à perdre haleine" },
-      { "104034": "Viens me retrouver" },
-      { "109034": "Si tu crois un jour que tu m'aimes" },
-      { "113034": "Et si ce jour-là tu as de la peine" },
-      { "118034": "À trouver où tous ces chemins te mènent" },
-      { "123034": "Viens me retrouver" },
-      { "130034": "Si le dégoût de la vie vient en toi" },
-      { "138033": "Si la paresse de la vie s'installe en toi" },
-      { "145034": "Pense à moi" },
-      { "149034": "Pense à moi" },
-      { "155034": "Mais si tu crois un jour que tu m'aimes" },
-      { "158034": "Ne le considère pas comme un problème" },
-      { "163034": "Et cours et cours jusqu'à perdre haleine" },
-      { "168034": "Viens me retrouver" },
-      { "173034": "Si tu crois un jour que tu m'aimes" },
-      { "177034": "N'attends pas un jour, pas une semaine" },
-      { "182034": "Car tu ne sais pas où la vie t'amène" },
-      { "187034": "Viens me retrouver" },
-      { "194034": "Si le dégoût de la vie vient en toi" },
-      { "202034": "Si la paresse de la vie s'installe en toi" },
-      { "209034": "Pense à moi" },
-      { "213034": "Pense à moi" },
+      { 90034: "Mais si tu crois un jour que tu m'aimes" },
+      { 94034: "Ne crois pas que tes souvenirs me gênent" },
+      { 99034: "Et cours, cours jusqu'à perdre haleine" },
+      { 104034: "Viens me retrouver" },
+      { 109034: "Si tu crois un jour que tu m'aimes" },
+      { 113034: "Et si ce jour-là tu as de la peine" },
+      { 118034: "À trouver où tous ces chemins te mènent" },
+      { 123034: "Viens me retrouver" },
+      { 130034: "Si le dégoût de la vie vient en toi" },
+      { 138033: "Si la paresse de la vie s'installe en toi" },
+      { 145034: "Pense à moi" },
+      { 149034: "Pense à moi" },
+      { 155034: "Mais si tu crois un jour que tu m'aimes" },
+      { 158034: "Ne le considère pas comme un problème" },
+      { 163034: "Et cours et cours jusqu'à perdre haleine" },
+      { 168034: "Viens me retrouver" },
+      { 173034: "Si tu crois un jour que tu m'aimes" },
+      { 177034: "N'attends pas un jour, pas une semaine" },
+      { 182034: "Car tu ne sais pas où la vie t'amène" },
+      { 187034: "Viens me retrouver" },
+      { 194034: "Si le dégoût de la vie vient en toi" },
+      { 202034: "Si la paresse de la vie s'installe en toi" },
+      { 209034: "Pense à moi" },
+      { 213034: "Pense à moi" },
     ],
   };
 
   if (videoState === "pause") {
     timeoutIds.forEach((id) => clearTimeout(id));
-    timeoutIds.length = 0; // Limpa o array para remover referências aos IDs
+    valoresAtuais.length = 0;
+    timeoutIds.length = 0;
     console.log("Pausou");
     console.log(time);
+    
     return time;
   }
 
-  console.log("teste1");
-  if (!passedOnce) {
-    console.log("teste2");
-    lyricsData.lyricsTimeVerse.forEach((lista) => {
-      const intervalId = setTimeout(() => {
-        time = Number(Object.keys(lista)[0]);
+  valoresAtuais.length = 0;
+  let contador = 0;
+
+  console.log(passedOnce);
+  lyricsData.lyricsTimeVerse.forEach((lista, index) => {
+    const intervalId = setTimeout(
+      () => {
+        console.log(index);
+        var key
+        if (key != time && passedOnce) {
+          key = time;
+        } else {
+          key = Object.keys(lista)[0];
+        }
+        console.log(key);
+        const verse = lista[key];
+        time = Number(key);
+        console.log(index);
+        lyricsPageId.innerHTML = "";
+        const words = verse.split(" ");
+
+        // Colocar verso no span
+        words.forEach((word) => {
+          contador += 1;
+          const chanceOfWrite = parseInt(Math.random() * 100 + 1);
+
+          const input = document.createElement("input");
+          input.id = `input-${contador}`;
+          input.setAttribute("data-word", word);
+
+          if (chanceOfWrite < 100) {
+            input.value = word;
+            lyricsPageId.appendChild(input);
+            input.disabled = true;
+          } else {
+            input.value = "";
+            lyricsPageId.appendChild(input);
+          }
+          valoresAtuais.push(input.value);
+          console.log(valoresAtuais);
+
+          input.addEventListener("keydown", () => {
+            checkInputs();
+          });
+        });
+
+        // Verifica se há palavras vazias que precisam ser preenchidas
+        if (valoresAtuais.includes("")) {
+          test("pause", true);
+          return;
+        }
+        // Se houver próximo verso, atualiza o tempo
+        if (index + 1 < lyricsData.lyricsTimeVerse.length) {
+          const nextVerse = lyricsData.lyricsTimeVerse[index + 1]; // Pega o próximo objeto do array
+          time = Number(Object.keys(nextVerse)[0]); // Pega o tempo do próximo verso
+          key = time;
+          console.log("Próximo tempo:", time);
+        }
+
         console.log(time);
-        lyricsPageId.innerHTML = "";
-        const words = lista[Object.keys(lista)[0]].split(" ");
-        
-        words.forEach((word) => {
-          var willWrite = parseInt(Math.random() * 10 + 1)
-          console.log(willWrite)
+      },
+      passedOnce
+        ? Number(Object.keys(lista)[0]) - time
+        : Number(Object.keys(lista)[0])
+    );
+    timeoutIds.push(intervalId);
+  });
+}
 
-            const span = document.createElement("span");
+function checkInputs() {
+  console.log(time);
+  const inputs = document.querySelectorAll("#lyricsPageId input");
+  let allCorrect = true;
 
-            if(willWrite < 9) {
-              span.textContent = word;
-              lyricsPageId.appendChild(span);
-              console.log(word)
-            } else {
-              span.textContent = "";
-              lyricsPageId.appendChild(span);
-              console.log(word)
-            }
-            
-        });
-      }, Object.keys(lista)[0]);
-      timeoutIds.push(intervalId);
-    });
-  } else if (passedOnce) {
-    console.log("teste3");
-    lyricsData.lyricsTimeVerse.forEach((lista) => {
-      const tempo = Number(Object.keys(lista)[0]);
+  inputs.forEach((input) => {
+    const correctWord = input.getAttribute("data-word");
+    if (input.value !== correctWord) {
+      allCorrect = false;
+    }
+  });
 
-      const intervalId = setTimeout(() => {
-        time = tempo; // atualizar tempo como tempo que pausou e tempo que começa. Depois faz a subtração
+  if (allCorrect) {
+    console.log("foi!"); // Prossegue o tempo do texto
+    console.log(time);
 
-        lyricsPageId.innerHTML = "";
-        const words = lista[tempo].split(" ");
-        words.forEach((word) => {
-          const span = document.createElement("span");
-          span.textContent = word;
-          lyricsPageId.appendChild(span);
-        });
-
-      }, tempo - time); // subtração. Tempo que passou - tempo que pausou
-
-      timeoutIds.push(intervalId);
-    });
+    test("playing", true);
+  } else {
+    console.log("não foi");
   }
-  // } else if (videoState != "pause" && passedOnce) {
-  //   console.log("teste3");
-  //   lyricsData.lyricsTimeVerse.forEach((lista) => {
-  //     console.log(Object.keys(lista)[0]);
-  //     const intervalId = setTimeout(() => {
-  //       console.log(timeoutIds);
-  //       console.log(timeoutIds[intervalId]);
-  //       time = Object.keys(lista)[0];
-  //       console.log(time);
-  //       lyricsPageId.innerHTML = "";
-  //       const words = lista[Object.keys(lista)[0]].split(" ");
-  //       words.forEach((word) => {
-  //         const span = document.createElement("span");
-  //         span.textContent = word;
-  //         lyricsPageId.appendChild(span);
-  //         console.log(timeoutIds);
-  //       });
-  //     }, Object.keys(lista)[0]);
-  //     timeoutIds.push(intervalId);
-  //   });
-  // }
 }
